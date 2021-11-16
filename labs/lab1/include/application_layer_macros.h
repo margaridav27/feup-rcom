1cfa072c25a2e34d41674b33d537e2d4acfb781c
Margarida Vieira <margaridajcv.27@gmail.com>
2021-11-16 20:57:43 +0000

    ctrl packet

--- labs/lab1/include/application_layer_macros.h
+++ labs/lab1/include/application_layer_macros.h
@@ -3,16 +3,14 @@
 
 #define BIT(n) (0x01 << (n))
 
-#define GET_N_BYTE_OF(x, n) (((x) >> ((n)*8)) & 0xFF)
-
 #define OCTET_SIZE 256
 
 /* ctrl & data packets */
-#define CTRL_IX    0
+#define CTRL_IX 0
 
 /* ctrl packet */
-#define CTRL_START 2 /* start data packet emission */
-#define CTRL_END   3 /* end data packet emission */
+#define CTRL_START 2     /* start data packet emission */
+#define CTRL_END 3       /* end data packet emission */
 #define DATA_FILE_SIZE 0 /* ctrl packet file size parameter identifier */
 #define DATA_FILE_NAME 1 /* ctrl packet file name parameter identifier */
 
@@ -23,6 +21,5 @@
 #define DATA_LENGTH_LSB_IDX 3
 #define DATA_START_IDX 4
 #define DATA_SEQ_NUM_SIZE 256
-
 
 #endif
--- labs/lab1/src/application_layer.c
+++ labs/lab1/src/application_layer.c
@@ -9,22 +9,25 @@
 typedef struct {
   char type;
   char length;
-  char *value;
+  char* value;
 } ctrl_packet_parameter_t;
 
 typedef struct {
-  char* filename;
-  int fileDescriptor;
+  char* file_name;
+  int file_descriptor;
   int status;
-  int filePortion;
+  int file_portion;
 } application_layer_t;
 
 application_layer_t application_layer;
 
-int init(char* filename, char* port) {
-  application_layer.filename = filename;
-  application_layer.fileDescriptor = open(filename, O_RDWR | O_NOCTTY | O_NONBLOCK);   //será que tem que ser fopen?
-  if (application_layer.fileDescriptor < 0) {
+int init(char* file_name, char* port) {
+  application_layer.file_name = file_name;
+  application_layer.file_descriptor =
+      open(file_name,
+           O_RDWR | O_NOCTTY | O_NONBLOCK);  // será que tem que ser fopen?
+
+  if (application_layer.file_descriptor < 0) {
     perror("open file");
     return -1;
   }
@@ -34,38 +37,38 @@ int init(char* filename, char* port) {
 
 int createCtrlPacket(char ctrl, char* packet) {
   struct stat st;
-  stat(application_layer.filename, &st);
+  stat(application_layer.file_name, &st);
 
   off_t size = st.st_size;
 
   ctrl_packet_parameter_t file_size = {
       .type = DATA_FILE_SIZE,
-      .length = OCTECT_SIZE % size + 1,
+      .length = OCTET_SIZE % size + 1,
       .value = malloc(file_size.length * sizeof(char))};
 
-  int nth_byte = file_size.length - 1; // starts at msb
+  int nth_byte = file_size.length - 1;  // starts at msb
   for (int i = 0; i < file_size.length; i++) {
-    file_size.value[i] = GET_N_BYTE_OF(size, nth_byte);
+    file_size.value[i] = (size >> (nth_byte * 8)) & 0xFF;  // get nth byte
     nth_byte--;
   }
 
-  size_t name_size = strlen(application_layer.filename) + 1;
+  size_t name_size = strlen(application_layer.file_name) + 1;
 
   ctrl_packet_parameter_t file_name = {
       .type = DATA_FILE_NAME,
       .length = OCTET_SIZE % name_size + 1,
       .value = malloc(file_name.length * sizeof(char))};
 
-  int nth_byte = file_name.length - 1; // starts at msb
-  for (int i = 0; i < file_size.length; i++) {
-    file_name.value[i] = GET_N_BYTE_OF(application_layer.filename, nth_byte);
+  int nth_byte = file_name.length - 1;  // starts at msb
+  for (int i = 0; i < file_name.length; i++) {
+    file_name.value[i] = application_layer.file_name[i];
     nth_byte--;
   }
 
   packet = malloc(1 + sizeof(file_size) + sizeof(file_name));
+  // TODO preencher efetivamente o packet com os values
+
   packet[CTRL_IX] = ctrl;
-  
-  //TODO preencher efetivamente o packet com os values
 
   return 0;
 }
@@ -76,37 +79,37 @@ int createDataPacket() {
 }
 
 // TODO #2
-int emmit(char* filename, char* port) {
-  init(filename, port);
+int emmit(char* file_name, char* port) {
+  init(file_name, port);
 
   // get size file
-  unsigned char buf[application_layer.filePortion], packet[application_layer.filePortion * 2];
+  unsigned char buf[application_layer.file_portion],
+      packet[application_layer.file_portion * 2];
 
   createCtrlPacket(CTRL_START, packet);
   llwrite(packet);
 
+  while (read(application_layer.file_descriptor, buf,
+              application_layer.file_portion) > 0) {
+    createDataPacket(buf, packet);
 
-  while (read(application_layer.fileDescriptor, buf, application_layer.filePortion) > 0) {
-      createDataPacket(buf, packet);
-      
-      llwrite(packet);
+    llwrite(packet);
   }
-
 
   createCtrlPacket(CTRL_END, packet);
   llwrite(packet);
 
-  llclose(filename, port);
+  llclose(file_name, port);
   return 0;
 }
 
-void end(char* filename, char* port) {
-  llclose(filename);
+void end(char* file_name, char* port) {
+  llclose(file_name);
 
   /* end communication by
     1. sending DISC ctrl frame
     2. waiting for DISC ctrl frame that will be sent by receiver
-    3. sending UA ctrl frame and effectively ending communication 
+    3. sending UA ctrl frame and effectively ending communication
   */
   termination();
 }
