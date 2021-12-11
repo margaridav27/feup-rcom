f37a15c75096c2639d7a18f10d008c0abfbfadc1
beatriz-ag <beatrizaguiar.up@gmail.com>
2021-12-11 16:47:07 +0000

    a

--- labs/lab1/src/application_layer.c
+++ labs/lab1/src/application_layer.c
@@ -61,7 +61,7 @@ int openFile() {
 int init(char* file_name, char* port) {
   setupAlarm();
 
-  application_layer.max_size_read = 2000;
+  application_layer.max_size_read = 5000;
   if (application_layer.status == TRANSMITER) {
     application_layer.file_name = file_name;
     openFile();
